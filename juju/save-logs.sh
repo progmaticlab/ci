e8583c7170b20eb6bf768232138e6099986e612e
Andrey Pavlov <andrey.mp@gmail.com>
2017-12-26 20:31:27 +0300

    fix logs saving

--- juju/save-logs.sh
+++ juju/save-logs.sh
@@ -26,8 +26,8 @@ cat <<EOS >$slf
 rm -f logs.*
 tar -cf logs.tar /var/log/juju 2>/dev/null
 for ldir in '/etc/apache2' '/etc/apt' '/etc/neutron' '/etc/nova' '/etc/haproxy' '/var/log/upstart' '/var/log/neutron' '/var/log/nova' '/etc/keystone' '/var/log/keystone' ; do
-  if [ -d "$ldir" ] ; then
-    tar -rf logs.tar "$ldir" 2>/dev/null
+  if [ -d "\$ldir" ] ; then
+    tar -rf logs.tar "\$ldir" 2>/dev/null
   fi
 done
 ps ax -H &> ps.log
