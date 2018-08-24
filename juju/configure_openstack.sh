7641587c500724758fd3362cc24daa448115bb5d
Andrey Pavlov <andrey.mp@gmail.com>
2018-08-24 22:33:19 +0300

    increase memory for nodes

--- juju/configure_openstack.sh
+++ juju/configure_openstack.sh
@@ -32,7 +32,7 @@ openstack subnet create --network public --subnet-range $public_network_addr.0/2
 
 openstack flavor create --ram 256 --vcpus 1 --public small
 
-rm cirros-0.3.5-x86_64-disk.img
+rm -f cirros-0.3.5-x86_64-disk.img
 wget -nv http://download.cirros-cloud.net/0.3.5/cirros-0.3.5-x86_64-disk.img
 openstack image create --public --file cirros-0.3.5-x86_64-disk.img cirros
 
